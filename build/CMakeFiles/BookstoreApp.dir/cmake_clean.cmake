0c9fdc75c484371986aa72aefffe254b025162d4
Prashant Jatoo <jatooprashant099@gmail.com>
2024-08-27 23:31:56 +0400

    Added instructions

---
+++ build/CMakeFiles/BookstoreApp.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "BookstoreApp"
+  "BookstoreApp.pdb"
+  "CMakeFiles/BookstoreApp.dir/src/appBookstore.cpp.o"
+  "CMakeFiles/BookstoreApp.dir/src/appBookstore.cpp.o.d"
+  "CMakeFiles/BookstoreApp.dir/src/dbops.cpp.o"
+  "CMakeFiles/BookstoreApp.dir/src/dbops.cpp.o.d"
+  "CMakeFiles/BookstoreApp.dir/src/io.cpp.o"
+  "CMakeFiles/BookstoreApp.dir/src/io.cpp.o.d"
+  "CMakeFiles/BookstoreApp.dir/src/library.cpp.o"
+  "CMakeFiles/BookstoreApp.dir/src/library.cpp.o.d"
+  "CMakeFiles/BookstoreApp.dir/src/user.cpp.o"
+  "CMakeFiles/BookstoreApp.dir/src/user.cpp.o.d"
+  "CMakeFiles/BookstoreApp.dir/src/userAuth.cpp.o"
+  "CMakeFiles/BookstoreApp.dir/src/userAuth.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BookstoreApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/BookstoreApp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BookstoreApp.
+# This may be replaced when dependencies are built.
