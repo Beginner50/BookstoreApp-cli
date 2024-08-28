777c030c102db3980af2078343a809805fbefa6b
Prashant Jatoo <118896436+Beginner50@users.noreply.github.com>
2024-08-28 09:16:51 +0400

    Update bookstoreDB.sql

--- db/bookstoreDB.sql
+++ db/bookstoreDB.sql
@@ -1,6 +1,5 @@
 -- Create Tables
 
-/*
 CREATE TABLE member(
 memID VARCHAR(4) UNIQUE,
 uname VARCHAR(18),
@@ -26,33 +25,13 @@ PRIMARY KEY(memID, bookID),
 CHECK(bookID LIKE('b%')),
 CHECK(memID LIKE('m%'))
 );
-*/
 
 -- sample values
 /*
-INSERT INTO member VALUES
-('m001', 'Francois', 'h3avenS3eker'),
-('m002', 'Petroy', 'Dmitri444'),
-('m003', 'Jiang', '123456');
-
 INSERT INTO book VALUES
 ('b000', 'Lord Of The Mysteries', true),
-('b001', 'Shadow Slave', true),
-('b002', 'Reverend Insanity', false),
-('b003', 'Florida Man', true),
 ('b005', 'Legendary Mechanic', true),
-('b006', 'Overgeared', true),
 ('b7', 'Overlord', true),
 ('b8', 'Astral Apostle', true),
 ('b007', 'Fire Mage', true);
-
-INSERT INTO borrow VALUES
-('m001', 'b001' ),
-('m002', 'b004' ),
-('m002', 'b003' )
-;
 */
-
-SELECT * FROM member;
-SELECT * FROM book;
-SELECT * FROM borrow;
